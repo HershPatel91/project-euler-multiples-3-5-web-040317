ac591aa7ec0e5fc8e86e05cf54e69cfd4a09f690
github username <github email address>
2017-04-17 09:28:06 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+require 'pry'
+
+
+def collect_multiples(num)
+	i = 1
+	arr = []
+	while i <num
+		arr << i
+		i += 1
+	end
+	arr.select do |num|
+		num % 5 == 0 || num % 3 ==0
+	end
+end
+
+def sum_multiples(num)
+	arr = collect_multiples(num)
+	arr.inject(0, :+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+attr_accessor :num
+
+def initialize(num)
+	@num = num
+end
+
+def collect_multiples
+	(1...self.num).select do |num|
+		num % 5 == 0 || num % 3 ==0
+	end
+end
+
+def sum_multiples
+	arr = collect_multiples
+	arr.inject(0, :+)
+end
+
+end
